1009b5c0563c906dbcf067a7bca96f1337e16d99
Victor Oliveira <victoroliveira@MacBook-Air-de-Victor.local>
2023-03-07 10:47:22 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp
+
+  ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Container(
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.red,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.blue,
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.blue,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.red,
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.cyan,
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      color: Colors.pinkAccent,
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      color: Colors.purple,
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                ),
+                Container(
+                  color: Colors.amber,
+                  height: 30,
+                  width: 300,
+                  child: Text(
+                    'Diamante Amarelo',
+                    style: TextStyle(color: Colors.black, fontSize: 28),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ElevatedButton(onPressed: () {
+                  print('Você apertou o Botão');
+                }, child: Text('Aperte o botão!')
+                )
+              ],
+            )
+        )
+    );
+  }
+}
