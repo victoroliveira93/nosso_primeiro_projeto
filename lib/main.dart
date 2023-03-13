d4f6e6ea6e9a0ab99bf41360cb32e3f15c44279f
Victor Oliveira <victoroliveira@MacBook-Air-de-Victor.local>
2023-03-13 10:34:01 -0300

    refatoracao

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nosso_primeiro_projeto/screens/initial_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,122 +15,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            leading: Container(),
-            title: Text('Tarefas'),
-          ),
-          body: ListView(
-            children: [
-              Task('Aprender Flutter'),
-              Task('Andar de Bike'),
-              Task('Meditar'),
-              Task('Ler'),
-              Task('Andar de Bike'),
-              Task('Meditar'),
-              Task('Aprender Flutter fwfwefwefwefwefwefwfwf'),
-              Task('Andar de Bike'),
-              Task('Meditar'),
-              Task('Aprender Flutter'),
-              Task('Andar de Bike'),
-              Task('Meditar'),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(onPressed: () {})),
+      home: const InitialScreen()
     );
   }
 }
 
-class Task extends StatefulWidget {
-  final String nome;
 
-  const Task(this.nome, {Key? key}) : super(key: key);
-
-  @override
-  State<Task> createState() => _TaskState();
-}
-
-class _TaskState extends State<Task> {
-  int nivel = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-          child: Stack(
-        children: [
-          Container(
-            color: Colors.blue,
-            height: 140,
-          ),
-          Column(
-            children: [
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
-                    Container(
-                      width: 200,
-                      child: Text(
-                        widget.nome,
-                        style: TextStyle(
-                            fontSize: 24, overflow: TextOverflow.ellipsis),
-                      ),
-                    ),
-                    Container(
-                      height: 52,
-                      width: 52,
-                      child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              nivel++;
-                            });
-
-                            print(nivel);
-                          },
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Icon(Icons.arrow_drop_up),
-                              Text('UP', style: TextStyle(fontSize: 12),)
-                            ],
-                          )),
-                    )
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                        width: 200,
-                        child: LinearProgressIndicator(
-                            color: Colors.white, value: nivel / 10)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Nível: $nivel',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          )
-        ],
-      )),
-    );
-  }
-}
